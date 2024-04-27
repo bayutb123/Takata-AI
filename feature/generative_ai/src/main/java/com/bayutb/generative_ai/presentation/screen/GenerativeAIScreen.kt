d7eb89e718c74e3accbeafc8edf1e65eeb1999dc
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2024-04-28 01:21:24 +0700

    feature: remove tooltip

--- feature/generative_ai/src/main/java/com/bayutb/generative_ai/presentation/screen/GenerativeAIScreen.kt
+++ feature/generative_ai/src/main/java/com/bayutb/generative_ai/presentation/screen/GenerativeAIScreen.kt
@@ -1,16 +1,12 @@
 package com.bayutb.generative_ai.presentation.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.PlainTooltipBox
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -19,10 +15,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.bayutb.generative_ai.presentation.screen.GenerativeAIDestinations
 import com.bayutb.generative_ai.presentation.viewmodel.GenerativeAIViewModel
 import com.bayutb.ui.component.input.MainChatInput
 import kotlinx.coroutines.launch
@@ -39,16 +33,7 @@ fun GenerativeAIScreen(
     Scaffold(
         topBar = {
             TopAppBar(title = {
-                Row {
-                    Text(text = "Takata AI")
-                    PlainTooltipBox(tooltip = { Text(text = "Generative AI") }, containerColor = Color.Gray) {
-                        Button(onClick = {
-                            onNavigationRequested(GenerativeAIDestinations.HOME)
-                        }) {
-                            Text(text = "Home")
-                        }
-                    }
-                }
+                Text(text = "Takata AI")
             })
         }
     ) { paddingValue ->
@@ -65,13 +50,15 @@ fun GenerativeAIScreen(
             ) {
                 Text(text = state)
             }
-            MainChatInput(onSendClick = {
-                scope.launch {
-                    generativeAIViewModel.generate()
-                }
-            }, modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp))
+            MainChatInput(
+                onSendClick = {
+                    scope.launch {
+                        generativeAIViewModel.generate()
+                    }
+                }, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
         }
     }
 }
